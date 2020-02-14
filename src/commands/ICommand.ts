06f605fea7df5f17b8c38b3ccdcd25ec99785c03
Nikola Anastasov Velichkov <federlizer@gmail.com>
2020-02-14 19:40:04 +0100

    make interface easily accessible

--- src/commands/ICommand.ts
+++
@@ -1,7 +0,0 @@
-import { Message } from 'discord.js';
-
-export default interface ICommand {
-  name: string;
-  description: string;
-  execute: (message: Message) => void;
-}
--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,4 +1,12 @@
+import { Message } from 'discord.js';
+
 import pingCommand from './ping';
+
+export interface ICommand {
+  name: string;
+  description: string;
+  execute: (message: Message) => void;
+};
 
 export default [
   pingCommand,
