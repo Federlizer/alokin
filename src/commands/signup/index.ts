adb8dbd1283cc72bcd0a11ea4112ce71866a9ea4
Nikola Anastasov Velichkov <federlizer@gmail.com>
2020-02-22 17:57:31 +0100

    use DB to persist participants

--- src/commands/signup/index.ts
+++ src/commands/signup/index.ts
@@ -9,9 +9,9 @@ import {
 
 import tournamentNames from './tournamentNames';
 
+import Tournament, { ITournament } from '../../models/Tournament';
 
 function execute(message: Message, _: string[]) {
-  const user: User = message.author;
   const channel: (TextChannel|DMChannel|GroupDMChannel) = message.channel;
 
   if (!(channel instanceof TextChannel)) {
@@ -19,11 +19,37 @@ function execute(message: Message, _: string[]) {
     return;
   }
 
+  const user: User = message.author;
   const channelName = channel.name;
 
-  user.createDM()
-    .then((channel: DMChannel) => {
-      return channel.send(`Hello, you just signed up for the ${tournamentNames[channelName]} tournament.`)
+  Tournament.findOne({ name: channelName })
+    .then((tournament: ITournament|null) => {
+      if (tournament === null ) return;
+
+      const signedUp = tournament.participants.find((p) => p === user.tag);
+      if (signedUp) {
+        user.createDM()
+          .then((channel: DMChannel) => {
+            const msg = 'Your tag is already registered for this tournament. If you haven\'t actually registered, please contact the tournament organizer for a resolution.';
+            return channel.send(msg);
+          })
+          .catch((err) => console.error(err));
+
+          return;
+      }
+
+      tournament.participants = [...tournament.participants, user.tag];
+      tournament.save()
+      .then(() => {
+        user.createDM()
+          .then((channel: DMChannel) => {
+            const msg = `You have been signed up for the ${tournamentNames[channelName]} tournament.`;
+            return channel.send(msg)
+          })
+          .catch((err) => console.error(err));
+      })
+      .catch((err) => console.error(err));
+
     })
     .catch((err) => console.error(err));
 }
