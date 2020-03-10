a98e98b87845dfac8438efebf94f417acff6b6fc
Nikola Anastasov Velichkov <federlizer@gmail.com>
2020-03-10 10:55:06 +0100

    include the full challonge url

--- src/challonge/ITournament.ts
+++ src/challonge/ITournament.ts
@@ -2,4 +2,5 @@ export default interface Tournament {
   id: number;
   name: string;
   tournament_type: ('single elimination'|'double elimination'|'round robin'|'swiss');
+  full_challonge_url: string;
 }
